ebd0743d46f3ef6eace56ba1e4a8d552121c0bf6
Ivan Foong <ivan.foong@docomointertouch.com>
2015-09-07 14:31:01 +0800

    Added sample test

---
+++ Example/Tests/Tests.swift
@@ -0,0 +1,21 @@
+//
+//  Tests.swift
+//  IFCacheKit
+//
+//  Created by Ivan Foong on 7/9/15.
+//  Copyright (c) 2015 CocoaPods. All rights reserved.
+//
+
+import Quick
+import Nimble
+import IFCacheKit
+
+class TestSpec: QuickSpec {
+    override func spec() {
+        describe("the memory cache") {
+            it("can put items") {
+                expect(1) == 1
+            }
+        }
+    }
+}
